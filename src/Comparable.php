7b3b9949f624d03f8a5af37a4e28685ae0425b6f
gossi <gossi@users.noreply.github.com>
2018-07-24 16:22:41 +0200

    More documentation/code samples and split endsWith and startsWith into two methods each

--- src/Comparable.php
+++ src/Comparable.php
@@ -7,10 +7,9 @@ interface Comparable {
 	 * Compares to another object
 	 *
 	 * @param mixed $comparison
-	 * @return int
-	 * 		Return Values:
-	 * 		< 0 if the object is less than comparison
-	 * 		> 0 if the object is greater than comparison
+	 * @return int Return Values:<br>
+	 * 		&lt; 0 if the object is less than comparison<br>
+	 *  	&gt; 0 if the object is greater than comparison<br>
 	 * 		0 if they are equal.
 	 */
 	public function compareTo($comparison);
