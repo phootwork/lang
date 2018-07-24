e8c2d32c6520850b084a6cc866f2150c3018bc09
gossi <gossi@users.noreply.github.com>
2018-07-24 16:22:49 +0200

    Updated sami

--- sami.php
+++ sami.php
@@ -2,13 +2,25 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Sami\Sami;
+use Sami\Parser\Filter\PublicFilter;
 use Symfony\Component\Finder\Finder;
 
 $iterator = Finder::create()->files()->name('*.php')->in(__DIR__ . '/src');
 
-return new Sami($iterator, [
+$sami = new Sami($iterator, [
 	'title' => 'Phootwork Lang API',
 	'theme' => 'default',
 	'build_dir' => __DIR__ . '/api',
-	'default_opened_level' => 2
+	'default_opened_level' => 2,
+	'sort_class_properties' => true,
+	'sort_class_methods' => true,
+	'sort_class_constants' => true,
+	'sort_class_traits' => true,
+	'sort_class_interfaces' => true
 ]);
+
+$sami['filter'] = function () {
+	return new PublicFilter();
+};
+
+return $sami;
