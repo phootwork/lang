cd93f94bc0fc4843523ac55c2ec9f16840828549
gossi <gossi@users.noreply.github.com>
2018-08-04 15:20:06 +0200

    merged methods from phootwork/collection into ArrayObject

--- src/ArrayObject.php
+++ src/ArrayObject.php
@@ -3,12 +3,17 @@ namespace phootwork\lang;
 
 class ArrayObject implements \ArrayAccess, \Countable, \IteratorAggregate, \Serializable, Arrayable {
 
-	private $array;
+	protected $array;
 
 	public function __construct($contents = []) {
 		$this->array = $contents;
 	}
 
+	/**
+	 * Counts the array
+	 *
+	 * @return int the amount of items
+	 */
 	public function count() {
 		return count($this->array);
 	}
@@ -28,16 +33,87 @@ class ArrayObject implements \ArrayAccess, \Countable, \IteratorAggregate, \Seri
 	}
 
 	/**
+	 * Resets the array
+	 *
+	 * @return $this
+	 */
+	public function clear() {
+		$this->array = [];
+		return $this;
+	}
+
+	/**
+	 * Checks whether this array is empty
+	 *
+	 * @return boolean
+	 */
+	public function isEmpty() {
+		return $this->count() === 0;
+	}
+
+	//
+	//
+	// MUTATIONS
+	//
+	//
+
+	/**
 	 * Push one or more elements onto the end of array
 	 *
 	 * @param mixed $_ values
 	 * @return $this
 	 */
 	public function push() {
+		// @TODO php7 $this->append(...$values);
 		// das ist doch behindi!
 		foreach (func_get_args() as $v) {
 			array_push($this->array, $v);
 		}
+		return $this;
+	}
+
+	/**
+	 * Append one or more elements onto the end of array
+	 *
+	 * @param mixed $_ values
+	 * @return $this
+	 */
+	public function append() {
+		// das ist doch behindi!
+		foreach (func_get_args() as $v) {
+			array_push($this->array, $v);
+		}
+		return $this;
+	}
+
+	/**
+	 * Adds an element to that array
+	 *
+	 * @param mixed $element
+	 * @param int $index
+	 * @return $this
+	 */
+	public function add($element, $index = null) {
+		if ($index === null) {
+			$this->array[$this->count()] = $element;
+		} else {
+			array_splice($this->array, $index, 0, $element);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Adds all elements to the array
+	 *
+	 * @param array|\Iterator $array
+	 * @return $this
+	 */
+	public function addAll($array) {
+		foreach ($array as $element) {
+			$this->add($element);
+		}
+
 		return $this;
 	}
 
@@ -51,7 +127,22 @@ class ArrayObject implements \ArrayAccess, \Countable, \IteratorAggregate, \Seri
 	}
 
 	/**
-	 * Prepend one or more elements to the beginning of an array
+	 * Prepend one or more elements to the beginning of the array
+	 *
+	 * @param mixed $_ values
+	 * @return $this
+	 */
+	public function unshift() {
+		// @TODO php7 $this->prepend(...$values);
+		// das ist doch auch behindi!
+		foreach (func_get_args() as $v) {
+			array_unshift($this->array, $v);
+		}
+		return $this;
+	}
+
+	/**
+	 * Prepend one or more elements to the beginning of the array
 	 *
 	 * @param mixed $_ values
 	 * @return $this
@@ -72,6 +163,55 @@ class ArrayObject implements \ArrayAccess, \Countable, \IteratorAggregate, \Seri
 	public function shift() {
 		return array_shift($this->array);
 	}
+
+	/**
+	 * Removes an element from the list
+	 *
+	 * @param mixed $element
+	 * @return $this
+	 */
+	public function remove($element) {
+		$index = array_search($element, $this->array, true);
+		if ($index !== null) {
+			unset($this->array[$index]);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Removes all elements from the list
+	 *
+	 * @param array|\Iterator $array
+	 * @return $this
+	 */
+	public function removeAll($array) {
+		foreach ($array as $element) {
+			$this->remove($element);
+		}
+
+		return $this;
+	}
+
+	/**
+	 * Remove a portion of the array and replace it with something else
+	 *
+	 * @param int $offset If offset is positive then the start of removed portion is at that offset from the beginning of the input array. If offset is negative then it starts that far from the end of the input array.
+	 * @param int $length If length is omitted, removes everything from offset to the end of the array. If length is specified and is positive, then that many elements will be removed. If length is specified and is negative then the end of the removed portion will be that many elements from the end of the array. If length is specified and is zero, no elements will be removed.
+	 * @param array $replacement If replacement array is specified, then the removed elements are replaced with elements from this array. If offset and length are such that nothing is removed, then the elements from the replacement array are inserted in the place specified by the offset. Note that keys in replacement array are not preserved. If replacement is just one element it is not necessary to put array() around it, unless the element is an array itself, an object or NULL.
+	 * @return $this
+	 */
+	public function splice($offset, $length = null, $replacement = []) {
+		$length = $length ?: $this->count();
+		$this->array = array_splice($this->array, $length, $replacement);
+		return $this;
+	}
+
+	//
+	//
+	// SORTING
+	//
+	//
 
 	/**
 	 * Sorts the array
@@ -112,22 +252,297 @@ class ArrayObject implements \ArrayAccess, \Countable, \IteratorAggregate, \Seri
 	/**
 	 * Internal sort function
 	 *
-	 * @param array $collection the collection on which is operated on
+	 * @param array $array the array on which is operated on
 	 * @param Comparator|callable $cmp the compare function
 	 * @param callable $usort the sort function for user passed $cmd
 	 * @param callable $sort the default sort function
 	 */
-	protected function doSort(&$collection, $cmp, callable $usort, callable $sort) {
+	protected function doSort(&$array, $cmp, callable $usort, callable $sort) {
 		if (is_callable($cmp)) {
-			$usort($collection, $cmp);
+			$usort($array, $cmp);
 		} else if ($cmp instanceof Comparator) {
-			$usort($collection, function($a, $b) use ($cmp) {
+			$usort($array, function($a, $b) use ($cmp) {
 				return $cmp->compare($a, $b);
 			});
 		} else {
-			$sort($collection);
+			$sort($array);
 		}
 	}
+
+	/**
+	 * Reverses the order of all elements
+	 *
+	 * @return $this
+	 */
+	public function reverse() {
+		$this->array = array_reverse($this->array);
+		return $this;
+	}
+
+	//
+	//
+	// SEARCH
+	//
+	//
+
+	/**
+	 * Tests whether all elements in the array pass the test implemented by the provided function.
+	 *
+	 * Returns <code>true</code> for an empty array.
+	 *
+	 * @param callable $callback
+	 * @return boolean
+	 */
+	public function every(callable $callback) {
+		$match = true;
+		foreach ($this->array as $element) {
+			$match = $match && $callback($element);
+		}
+
+		return $match;
+	}
+
+	/**
+	 * Tests whether at least one element in the array passes the test implemented by the provided function.
+	 *
+	 * Returns <code>false</code> for an empty array.
+	 *
+	 * @param callable $callback
+	 * @return boolean
+	 */
+	public function some(callable $callback) {
+		$match = false;
+		foreach ($this->array as $element) {
+			$match = $match || $callback($element);
+		}
+
+		return $match;
+	}
+
+	/**
+	 * Searches the array for query using the callback function on each element
+	 *
+	 * The callback function takes one or two parameters:
+	 *
+	 *     function ($element [, $query]) {}
+	 *
+	 * The callback must return a boolean
+	 *
+	 * @param mixed $query (optional)
+	 * @param callable $callback
+	 * @return boolean
+	 */
+	public function search() {
+		if (func_num_args() == 1) {
+			$callback = func_get_arg(0);
+		} else {
+			$query = func_get_arg(0);
+			$callback = func_get_arg(1);
+		}
+
+		foreach ($this->array as $element) {
+			$return = func_num_args() == 1 ? $callback($element) : $callback($element, $query);
+
+			if ($return) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Returns the element at the given index (or nothing if the index isn't present)
+	 *
+	 * @param int $index
+	 * @return mixed
+	 */
+	public function get($index) {
+		if (isset($this->array[$index])) {
+			return $this->array[$index];
+		}
+	}
+
+	/**
+	 * Searches the array with a given callback and returns the first element if found.
+	 *
+	 * The callback function takes one or two parameters:
+	 *
+	 *     function ($element [, $query]) {}
+	 *
+	 * The callback must return a boolean
+	 *
+	 * @param mixed $query OPTIONAL the provided query
+	 * @param callable $callback the callback function
+	 * @return mixed|null the found element or null if it hasn't been found
+	 */
+	public function find() {
+		if (func_num_args() == 1) {
+			$callback = func_get_arg(0);
+		} else {
+			$query = func_get_arg(0);
+			$callback = func_get_arg(1);
+		}
+
+		foreach ($this->array as $element) {
+			$return = func_num_args() == 1 ? $callback($element) : $callback($element, $query);
+
+			if ($return) {
+				return $element;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Searches the array with a given callback and returns the last element if found.
+	 *
+	 * The callback function takes one or two parameters:
+	 *
+	 *     function ($element [, $query]) {}
+	 *
+	 * The callback must return a boolean
+	 *
+	 * @param mixed $query OPTIONAL the provided query
+	 * @param callable $callback the callback function
+	 * @return mixed|null the found element or null if it hasn't been found
+	 */
+	public function findLast() {
+		if (func_num_args() == 1) {
+			$callback = func_get_arg(0);
+		} else {
+			$query = func_get_arg(0);
+			$callback = func_get_arg(1);
+		}
+
+		$reverse = array_reverse($this->array, true);
+		foreach ($reverse as $element) {
+			$return = func_num_args() == 1 ? $callback($element) : $callback($element, $query);
+
+			if ($return) {
+				return $element;
+			}
+		}
+
+		return null;
+	}
+
+	/**
+	 * Searches the array with a given callback and returns all matching elements.
+	 *
+	 * The callback function takes one or two parameters:
+	 *
+	 *     function ($element [, $query]) {}
+	 *
+	 * The callback must return a boolean
+	 *
+	 * @param mixed $query OPTIONAL the provided query
+	 * @param callable $callback the callback function
+	 * @return mixed|null the found element or null if it hasn't been found
+	 */
+	public function findAll() {
+		if (func_num_args() == 1) {
+			$callback = func_get_arg(0);
+		} else {
+			$query = func_get_arg(0);
+			$callback = func_get_arg(1);
+		}
+
+		$array = [];
+		foreach ($this->array as $k => $element) {
+			$return = func_num_args() == 1 ? $callback($element) : $callback($element, $query);
+
+			if ($return) {
+				$array[$k] = $element;
+			}
+		}
+
+		return new static($array);
+	}
+
+	/**
+	 * Returns the index of the given element or false if the element can't be found
+	 *
+	 * @param mixed $element
+	 * @return int the index for the given element
+	 */
+	public function indexOf($element) {
+		return array_search($element, $this->array, true);
+	}
+
+	/**
+	 * Searches the array with a given callback and returns the index for the first element if found.
+	 *
+	 * The callback function takes one or two parameters:
+	 *
+	 *     function ($element [, $query]) {}
+	 *
+	 * The callback must return a boolean
+	 *
+	 * @param mixed $query OPTIONAL the provided query
+	 * @param callable $callback the callback function
+	 * @return int|null the index or null if it hasn't been found
+	 */
+	public function findIndex() {
+		if (func_num_args() == 1) {
+			$callback = func_get_arg(0);
+		} else {
+			$query = func_get_arg(0);
+			$callback = func_get_arg(1);
+		}
+
+		$index = func_num_args() == 1 ? $this->find($callback) : $this->find($query, $callback);
+		if ($index !== null) {
+			$index = $this->indexOf($index);
+		}
+
+		return $index;
+	}
+
+	/**
+	 * Searches the array with a given callback and returns the index for the last element if found.
+	 *
+	 * The callback function takes one or two parameters:
+	 *
+	 *     function ($element [, $query]) {}
+	 *
+	 * The callback must return a boolean
+	 *
+	 * @param mixed $query OPTIONAL the provided query
+	 * @param callable $callback the callback function
+	 * @return int|null the index or null if it hasn't been found
+	 */
+	public function findLastIndex() {
+		if (func_num_args() == 1) {
+			$callback = func_get_arg(0);
+		} else {
+			$query = func_get_arg(0);
+			$callback = func_get_arg(1);
+		}
+
+		$index = func_num_args() == 1 ? $this->findLast($callback) : $this->findLast($query, $callback);
+		if ($index !== null) {
+			$index = $this->indexOf($index);
+		}
+
+		return $index;
+	}
+
+	/**
+	 * Checks whether the given element is in this array
+	 *
+	 * @param mixed $element
+	 * @return boolean
+	 */
+	public function contains($element) {
+		return in_array($element, $this->array, true);
+	}
+
+	//
+	//
+	// SUGAR
+	//
+	//
 
 	/**
 	 * Joins the array with a string
@@ -139,6 +554,18 @@ class ArrayObject implements \ArrayAccess, \Countable, \IteratorAggregate, \Seri
 	 */
 	public function join($glue = '') {
 		return new Text(implode($this->array, $glue));
+	}
+
+	/**
+	 * Extract a slice of the array
+	 *
+	 * @param int $offset
+	 * @param int $length
+	 * @param boolean $preserveKeys
+	 * @return ArrayObject
+	 */
+	public function slice($offset, $length = null, $preserveKeys = false) {
+		return new ArrayObject(array_slice($this->array, $offset, $length, $preserveKeys));
 	}
 
 	/**
@@ -174,6 +601,17 @@ class ArrayObject implements \ArrayAccess, \Countable, \IteratorAggregate, \Seri
 	 */
 	public function reduce(callable $callback, $initial = null) {
 		return array_reduce($this->array, $callback, $initial);
+	}
+
+	/**
+	 * Iterates the array and calls the callback function with the current item as parameter
+	 *
+	 * @param callable $callback
+	 */
+	public function each(callable $callback) {
+		foreach ($this->array as $item) {
+			$callback($item);
+		}
 	}
 
 	/**
@@ -223,6 +661,12 @@ class ArrayObject implements \ArrayAccess, \Countable, \IteratorAggregate, \Seri
 	public function toArray() {
 		return $this->array;
 	}
+
+	//
+	//
+	// INTERNALS
+	//
+	//
 
 	/**
 	 * @internal
--- tests/ArrayTest.php
+++ tests/ArrayTest.php
@@ -8,27 +8,27 @@ use phootwork\lang\ComparableComparator;
 use phootwork\lang\StringComparator;
 
 class ArrayTest extends \PHPUnit_Framework_TestCase {
-	
+
 	public function testArray() {
 		$base = ['a' => 'b', 'c' => 'd'];
 		$arr = new ArrayObject($base);
-	
+
 		$this->assertEquals(new ArrayObject(['b', 'd']), $arr->values());
 		$this->assertEquals(new ArrayObject(['a', 'c']), $arr->keys());
 		$this->assertEquals($base, $arr->toArray());
-	
+
 		$new = [];
 		foreach ($arr as $k => $v) {
 			$new[$k] = $v;
 		}
 		$this->assertEquals($new, $arr->toArray());
-	
+
 		$this->assertEquals(new ArrayObject(['b' => 'a', 'd' => 'c']), $arr->flip());
-		
+
 		$arr = new ArrayObject(['these', 'are', 'my', 'items']);
 		$this->assertEquals(new Text('these are my items'), $arr->join(' '));
 	}
-	
+
 	public function testCount() {
 		$arr = new ArrayObject(['these', 'are', 'my', 'items']);
 
@@ -39,10 +39,10 @@ class ArrayTest extends \PHPUnit_Framework_TestCase {
 
 		$this->assertEquals(6, $arr->count());
 	}
-	
+
 	public function testArrayAccess() {
 		$arr = new ArrayObject(['a' => 'b', 'c' => 'd']);
-		
+
 		$this->assertEquals('b', $arr['a']);
 		$this->assertTrue(isset($arr['c']));
 		$this->assertFalse(isset($arr['x']));
@@ -51,24 +51,24 @@ class ArrayTest extends \PHPUnit_Framework_TestCase {
 		$arr['a'] = 'x';
 		$this->assertEquals('x', $arr['a']);
 	}
-	
+
 	public function testSerialization() {
 		$arr = new ArrayObject(['these', 'are', 'my', 'items']);
 		$serialized = $arr->serialize();
-		
+
 		$brr = new ArrayObject();
 		$brr->unserialize($serialized);
-		
+
 		$this->assertEquals($arr, $brr);
 	}
-	
+
 	public function testReduce() {
 		$list = new ArrayObject(range(1, 10));
 		$sum = $list->reduce(function($a, $b) {return $a + $b;});
-	
+
 		$this->assertEquals(55, $sum);
 	}
-	
+
 	public function testFilter() {
 		$arr = new ArrayObject(['a' => 'a', 'b' => 'b', 'c' => 'c']);
 		$arr = $arr->filter(function ($item) {
@@ -77,22 +77,22 @@ class ArrayTest extends \PHPUnit_Framework_TestCase {
 
 		$this->assertSame(['a' => 'a', 'c' => 'c'], $arr->toArray());
 	}
-	
+
 	public function testMap() {
 		$arr = new ArrayObject(['a' => 'a', 'b' => 'b', 'c' => 'c']);
 		$arr = $arr->map(function ($item) {
 			return $item . 'val';
 		});
-	
+
 		$this->assertSame(['a' => 'aval', 'b' => 'bval', 'c' => 'cval'], $arr->toArray());
 	}
-	
+
 	public function testSort() {
 		$unsorted = [5, 2, 8, 3, 9, 4, 6, 1, 7, 10];
 		$list = new ArrayObject($unsorted);
 
 		$this->assertEquals(range(1, 10), $list->sort()->toArray());
-		
+
 		$list = new ArrayObject($unsorted);
 		$cmp = function ($a, $b) {
 			if ($a == $b) {
@@ -101,7 +101,7 @@ class ArrayTest extends \PHPUnit_Framework_TestCase {
 			return ($a < $b) ? -1 : 1;
 		};
 		$this->assertEquals(range(1, 10), $list->sort($cmp)->toArray());
-		
+
 		$items = ['x', 'c', 'a', 't', 'm'];
 		$list = new ArrayObject();
 		foreach ($items as $item) {
@@ -110,12 +110,12 @@ class ArrayTest extends \PHPUnit_Framework_TestCase {
 		$list->sort(new ComparableComparator());
 		$this->assertEquals(['a', 'c', 'm', 't', 'x'], $list->map(function ($item) {return $item->getContent();})->toArray());
 	}
-	
+
 	public function testSortAssoc() {
 		$arr = new ArrayObject(['b' => 'bval', 'a' => 'aval', 'c' => 'cval']);
 		$arr->sortAssoc();
 		$this->assertEquals(['a' => 'aval', 'b' => 'bval', 'c' => 'cval'], $arr->toArray());
-	
+
 		$arr = new ArrayObject(['b' => 'bval', 'a' => 'aval', 'c' => 'cval']);
 		$arr->sortAssoc(function ($a, $b) {
 			if ($a == $b) {
@@ -124,19 +124,19 @@ class ArrayTest extends \PHPUnit_Framework_TestCase {
 			return ($a < $b) ? -1 : 1;
 		});
 		$this->assertEquals(['a' => 'aval', 'b' => 'bval', 'c' => 'cval'], $arr->toArray());
-	
+
 		$arr = new ArrayObject(['b' => new Item('bval'), 'a' => new Item('aval'), 'c' => new Item('cval')]);
 		$arr->sortAssoc(new ComparableComparator());
 		$this->assertEquals(['a' => 'aval', 'b' => 'bval', 'c' => 'cval'], $arr
 				->map(function ($elem) {return $elem->getContent();})
 				->toArray());
 	}
-	
+
 	public function testSortKeys() {
 		$arr = new ArrayObject(['b' => 'bval', 'a' => 'aval', 'c' => 'cval']);
 		$arr->sortKeys();
 		$this->assertEquals(['a' => 'aval', 'b' => 'bval', 'c' => 'cval'], $arr->toArray());
-	
+
 		$arr = new ArrayObject(['b' => 'bval', 'a' => 'aval', 'c' => 'cval']);
 		$arr->sortKeys(function ($a, $b) {
 			if ($a == $b) {
@@ -145,17 +145,17 @@ class ArrayTest extends \PHPUnit_Framework_TestCase {
 			return ($a < $b) ? -1 : 1;
 		});
 		$this->assertEquals(['a' => 'aval', 'b' => 'bval', 'c' => 'cval'], $arr->toArray());
-	
+
 		$arr = new ArrayObject(['b' => 'bval', 'a' => 'aval', 'c' => 'cval']);
 		$arr->sortKeys(new StringComparator());
 		$this->assertEquals(['a' => 'aval', 'b' => 'bval', 'c' => 'cval'], $arr->toArray());
 	}
-	
+
 	public function testMutators() {
 		$base = ['b', 'c', 'd'];
 		$arr = new ArrayObject($base);
 		$arr->push('e', 'f');
-		
+
 		$this->assertEquals(['b', 'c', 'd', 'e', 'f'], $arr->toArray());
 		$this->assertEquals('f', $arr->pop());
 		$this->assertEquals('e', $arr->pop());
@@ -164,5 +164,128 @@ class ArrayTest extends \PHPUnit_Framework_TestCase {
 		$this->assertEquals('a', $arr->shift());
 		$this->assertEquals($base, $arr->toArray());
 	}
-	
+
+	public function testEach() {
+		$result = [];
+		$list = new ArrayObject(range(1, 10));
+		$list->each(function ($value) use (&$result) {
+			$result[] = $value;
+		});
+		$this->assertEquals($list->toArray(), $result);
+	}
+
+	public function testIndex() {
+		$item1 = 'item 1';
+		$item2 = 'item 2';
+		$item3 = 'item 3';
+		$items = [$item1, $item2];
+
+		$list = new ArrayObject($items);
+
+		$index1 = $list->indexOf($item1);
+		$this->assertEquals(0, $index1);
+		$this->assertEquals(1, $list->indexOf($item2));
+		$this->assertFalse($list->indexOf($item3));
+
+		$list->removeAll($items);
+		$list->addAll($items);
+
+		$this->assertEquals(2, $list->count());
+		$this->assertEquals($index1, $list->indexOf($item1));
+
+		$list->add($item3, 1);
+		$this->assertEquals($item3, $list->get(1));
+		$this->assertEquals($item2, $list->get(2));
+	}
+
+	public function testContains() {
+		$item1 = 'item 1';
+		$item2 = 'item 2';
+		$item3 = 'item 3';
+		$items = [$item1, $item2];
+
+		$list = new ArrayObject($items);
+
+		$this->assertTrue($list->contains($item2));
+		$this->assertFalse($list->contains($item3));
+	}
+
+	public function testFind() {
+		$list = new ArrayObject(range(1, 10));
+		$list = $list->map(function ($item) {
+			return new Item($item);
+		});
+
+		$search = function ($i, $query) {
+			return $i->getContent() == $query;
+		};
+
+		$item = $list->find(4, $search);
+		$this->assertTrue($item instanceof Item);
+		$this->assertEquals(4, $item->getContent());
+		$this->assertEquals(3, $list->findIndex(4, $search));
+		$this->assertNull($list->find(20, $search));
+
+		$fruits = new ArrayObject(['apple', 'banana', 'pine', 'banana', 'ananas']);
+		$fruits = $fruits->map(function ($item) {
+			return new Item($item);
+		});
+		$this->assertEquals(1, $fruits->findIndex(function ($elem) {
+			return $elem->getContent() == 'banana';
+		}));
+		$this->assertEquals(3, $fruits->findLastIndex(function ($elem) {
+			return $elem->getContent() == 'banana';
+		}));
+		$this->assertEquals(3, $fruits->findLastIndex('banana', function ($elem, $query) {
+			return $elem->getContent() == $query;
+		}));
+		$this->assertNull($fruits->findLast('mango', function ($elem, $query) {
+			return $elem->getContent() == $query;
+		}));
+
+		$apples = $fruits->findAll('apple', function ($elem, $query) {
+			return $elem->getContent() == $query;
+		});
+		$this->assertEquals(1, $apples->count());
+
+		$bananas = $fruits->findAll(function ($elem) {
+			return $elem->getContent() == 'banana';
+		});
+		$this->assertEquals(2, $bananas->count());
+	}
+
+	public function testSome() {
+		$list = new ArrayObject(range(1, 10));
+
+		$this->assertTrue($list->some(function ($item) {
+			return $item % 2 === 0;
+		}));
+
+		$this->assertFalse($list->some(function ($item) {
+			return $item > 10;
+		}));
+
+		$list = new ArrayObject();
+		$this->assertFalse($list->some(function () {
+			return true;
+		}));
+	}
+
+	public function testEvery() {
+		$list = new ArrayObject(range(1, 10));
+
+		$this->assertTrue($list->every(function ($item) {
+			return $item <= 10;
+		}));
+
+		$this->assertFalse($list->every(function ($item) {
+			return $item > 10;
+		}));
+
+		$list = new ArrayObject();
+		$this->assertTrue($list->every(function () {
+			return true;
+		}));
+	}
+
 }
